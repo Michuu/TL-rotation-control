00692037d581fc8764d406ad7bae6a40ce07ec2d
Michał Parniak <m.parniak@cent.uw.edu.pl>
2021-03-16 21:01:24 +0100

    better examples in main2

--- main2.py
+++ main2.py
@@ -5,12 +5,8 @@ ports = find_ports()
 #%%
 motor=Motor(ports[0].device)
 # %%
-pos=motor.get_('position')
-pos[1]/(motor.counts_per_rev//motor.range)
-# %%
-motor.do_('absolute',motor.deg_to_hex(5))
-# %%
-motor.do_('home')
-# %%
-motor.deg_to_hex(5)
-# %%
+motor.move_absolute(360)
+#%%
+motor.get_position()
+#%%
+motor.home()
